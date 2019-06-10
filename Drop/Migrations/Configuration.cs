415c9b2aeac687dc22d49c6c45817190c5c36514
Cristina-Andreea PAVEL <cristina.pavel1611@stud.acs.upb.ro>
2019-06-10 19:04:54 +0300

    bootstrap

--- Drop/Migrations/Configuration.cs
+++ Drop/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace Drop.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Drop.Models.ApplicationDbContext context)
--- Drop/SqlViews/DateUtilizator.cs
+++ Drop/SqlViews/DateUtilizator.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace Drop.SqlViews
 {
+    [Table("DateUtilizator")]
     public class DateUtilizator
     {
-        public string Id { get; set; }
+        [Key]
+        public Guid IdUtilizator { get; set; }
+
+        public string Nume { get; set; }
+
+        public string Prenume { get; set; }
+
+        public string Oras { get; set; }
+
+        public string Sex { get; set; }
+
+        public string Varsta { get; set; }
+
+        public string Greutate { get; set; }
+
+        public string GrupaSanguina { get; set; }
+
+        public string Rh { get; set; }
+
     }
 }
